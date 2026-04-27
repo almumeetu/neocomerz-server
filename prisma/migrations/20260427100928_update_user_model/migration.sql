6c71cd74121f9a3ff97db7285cf3ca61bf04be64
Md Abdul Ahad Linkon <m.alinkon10@gmail.com>
2026-04-27 16:12:49 +0600

    fix:user table data

---
+++ prisma/migrations/20260427100928_update_user_model/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('admin', 'customer', 'staff');
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ADD COLUMN     "password" TEXT NOT NULL,
+ADD COLUMN     "phone" TEXT,
+ADD COLUMN     "role" "Role" NOT NULL DEFAULT 'customer',
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "name" SET NOT NULL,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
