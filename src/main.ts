524627038ac351c8a63429e2680ae91c6ef7e337
Md Abdul Ahad Linkon <m.alinkon10@gmail.com>
2026-05-04 11:22:52 +0600

    fix; api versioning

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import { VersioningType } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
 
   const config = new DocumentBuilder()
     .setTitle('NeoComerz API')
@@ -13,7 +20,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
